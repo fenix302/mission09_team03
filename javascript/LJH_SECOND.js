312c2d72677eaa4373769274b128da3e0bafc8ed
lucelin0414 <lucelin0414@gmail.com>
2022-12-26 22:06:56 +0900

    ver1.01(LJH)

--- javascript/LJH_SECOND.js
+++ javascript/LJH_SECOND.js
@@ -1,32 +1,25 @@
 
 
 jQuery(document).ready(function(){
-    $('.navi > li').mouseover(function(){
+    $('.navbar-nav > li').mouseover(function(){
         $(this).find('.submenu').stop().slideDown(500);  
     }).mouseout(function(){
         $(this).find('.submenu').stop().slideUp(500);
     });
-
-    $('.imgslide > a:gt(0)').hide();
-    setInterval(function(){
-        $('.imgslide a:first-child').fadeOut().next('a').fadeIn().end().appendTo('.imgslide');},3000);
-    $(function(){
-        $('.tabmenu > li > a').click(function(){
-            $(this).parent().addClass('active')
-            .siblings().removeClass('active')
-            return false;
-        });
-    });
-
-    $('.notice li:first').click(function(){
-        $('#layer').addClass('active');
-    });
-    $('.btn').click(function(){
-        $('#layer').removeClass('active');
-    })
 });
 
+var re1 = document.getElementsByClassName("submenu");
+console.log(re1);
 
+window.onresize = function(){
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    if(width <= "1200"){
+        resize.mouseover.stop();
+    }
+
+}
 
 
 
