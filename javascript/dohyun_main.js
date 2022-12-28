85829f73b2f4aa3095c9d7e6b088892af42a4645
fasfaff <ehgus40582188@gmail.com>
2022-12-29 07:22:51 +0900

    ldh4

---
+++ javascript/dohyun_main.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+  var options = { videoId: 'RyvDXwKsghI', mute:true };
+  $('.mainvideo').tubular(options);
+  // f-UGhWj1xww cool sepia hd
+  // 49SKbS7Xwf4 beautiful barn sepia
+});
