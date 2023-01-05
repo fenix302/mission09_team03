3ba86acc3282a52fc419072a3171aa3073dd09a0
fenix302 <fenix302@naver.com>
2023-01-05 18:15:36 +0900

    jinho_9

--- javascript/choijinho_main.js
+++ javascript/choijinho_main.js
@@ -1,9 +1,7 @@
 $(function(){
-    // 쿠키("popup")의 값이 'none'이면 id값이 'notice_wrap'인 요소를 숨깁니다.
     if($.cookie('popup') == 'none'){
         $("#notice_wrap").hide();
     }
-    // class 값이 'closeBtn'인 요소를 클릭하면 체크박스의 체크 유무에 따라 쿠키를 생성하여 3일간 저장합니다.
     var $expiresChk = $("#expiresChk");
     $(".closeBtn").on("click", closePop);
     function closePop(){
