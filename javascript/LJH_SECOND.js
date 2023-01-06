968540002a9ff857809b2baebe24d8b5f6b65015
lucelin0414 <lucelin0414@gmail.com>
2023-01-06 14:41:45 +0900

    ver1.07(LJH)

--- javascript/LJH_SECOND.js
+++ javascript/LJH_SECOND.js
@@ -29,7 +29,23 @@ $(function(){
     });
 });
 
-
+$(function(){
+    $('.bxslider').bxSlider({
+        slideWidth: 1920,
+        // 이미지 위에 텍스트 올리기
+        captions: true,
+        // 슬라이드 전환 지속 시간(다음 슬라이드로 전환되는 과정에서의 사이 시간)
+        speed: 1000,
+        // 다음 전환까지 멈춰있는 시간(다음 슬라이드로 전환되기 전에 잠깐 멈춤 시간)
+        pause: 3000,
+        // 자동 실행
+        auto: true,
+        // 컨트롤 버튼(재생/정지) 표시
+        autoControls:false,
+        // 클릭해서 자동 실행 멈출 수 있도록 지정
+        stopAutoOnclick:false,
+    });
+});
 
 
 
